912660d77e121dacdf8a95fb9deac860e743b816
habib abdel <habib.abdelgaber@yahoo.com>
2025-02-13 18:43:24 +0200

    first commit - sprint-4 project 4 stage-1

---
+++ scripts/index.js
@@ -0,0 +1,44 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
+
+const cardList = document.querySelector(".cards__list");
+cardList.innerHTML = "";
+
+for (let card of initialCards) {
+  const cardElement = document.createElement("li");
+  cardElement.classList.add("card");
+
+  cardElement.innerHTML = `
+    <img src="${card.link}" alt="${card.name}" class="card__image" />
+    <div class="card__item">
+      <h3 class="card__title">${card.name}</h3>
+      <button class="card__like-button"></button>
+    </div>
+  `;
+
+  cardList.appendChild(cardElement);
+}
