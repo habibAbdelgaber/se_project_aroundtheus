d4b29b4aea3e5c24c273296b9b9b2a7690617dec
habib abdel <habib.abdelgaber@yahoo.com>
2025-02-25 12:40:59 +0200

    update sprint-4 project 4 stage-4 final

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## A gallery Website – A Modern Visual Showcase
 
-Welcome to my Gallery Website, a visually stunning platform designed with advanced HTML and CSS. This project showcases a sleek and responsive design, ensuring a seamless browsing experience across all devices.
+Welcome to my Gallery Website, a visually stunning platform designed with advanced HTML, CSS, and Javascript. This project showcases a sleek and responsive design, ensuring a seamless browsing experience across all devices.
 
-The gallery features a modern layout, and an intuitive user interface, making it perfect for displaying images in an elegant and immersive way. While the website currently focuses on design, I am working on adding functionality to enhance user interaction, such as filtering, zooming, and dynamic content updates.
+The gallery features a modern layout, and an intuitive user interface, making it perfect for displaying images in an elegant and immersive way. Toggling modal form and dynamically update profile content by inserting appropriate required data.
 
 Stay tuned for more updates as I continue improving and transforming this gallery into a fully interactive experience! 🚀
 
@@ -21,3 +21,6 @@ Stay tuned for more updates as I continue improving and transforming this galler
 [View the gallery](https://gallery.pymastery.com/)
 
 [Around the U.S](https://vimeo.com/1052391061/27c08d2eae)
+
+![Open Modal Button](./images/modal-form-features-1.jpg)
+![Profile Update Form](./images/modal-form-features-2.jpg)
--- scripts/index.js
+++ scripts/index.js
@@ -25,35 +25,81 @@ const initialCards = [
   },
 ];
 
+// Card list element container
 const cardList = document.querySelector(".cards__list");
-cardList.innerHTML = "";
 
-for (let card of initialCards) {
-  const cardElement = document.createElement("li");
-  cardElement.classList.add("card");
+// Template element
+const cardTemplate = document.querySelector("#card-template").content;
 
-  cardElement.innerHTML = `
-    <img src="${card.link}" alt="${card.name}" class="card__image" />
-    <div class="card__footer">
-      <h3 class="card__title">${card.name}</h3>
-      <button class="card__like-button"></button>
-    </div>
-  `;
+// Function to create a card element from a data object
+function getCardElement(data) {
+  // Clonimg the template
+  const cardElement = cardTemplate.cloneNode(true);
 
-  cardList.appendChild(cardElement);
+  // Clone the elements inside the card element
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+
+  return cardElement;
 }
 
-// Add event listener to open and close modal buttons
+// Clear existing content
+cardList.innerHTML = "";
 
-// Modal Functionality
+// Looping through the initial cards and append them to the list
+initialCards.forEach((card) => {
+  const cardElement = getCardElement(card);
+  cardList.appendChild(cardElement);
+});
+
+// Modal variables
 const modal = document.querySelector(".modal");
 const profileEditButton = document.querySelector(".content__edit-button");
 const closeModalButton = document.querySelector(".modal__close");
 
-profileEditButton.addEventListener("click", () => {
-  modal.style.display = "block";
-});
+function toggleModal() {
+  // Toggling modal on and off
+  modal.classList.toggle("open-modal");
+}
 
-closeModalButton.addEventListener("click", () => {
-  modal.style.display = "none";
-});
+// Add event listener to open the modal
+profileEditButton.addEventListener("click", toggleModal);
+
+// Profile variables
+const profileContentTitle = document.querySelector(".content__title");
+const profileContentDescription = document.querySelector(
+  ".content__description"
+);
+
+// Input fields
+const formInputName = document.querySelector(".form__input-name");
+const formInputDescription = document.querySelector(".form__input-description");
+
+// Form element
+const formElement = document.querySelector(".form");
+
+// Function to get the input fields with the current profile data
+function getProfileData() {
+  formInputName.value = profileContentTitle.textContent;
+  formInputDescription.value = profileContentDescription.textContent;
+}
+
+// Function to update the profile when form is submitted
+function updateProfileData(event) {
+  event.preventDefault();
+
+  profileContentTitle.textContent = formInputName.value;
+  profileContentDescription.textContent = formInputDescription.value;
+  toggleModal(); // Close the modal after updating the profile data
+}
+
+// Get the form fields when the form is opened
+document.addEventListener("DOMContentLoaded", getProfileData);
+
+// Add event listener to form submission
+formElement.addEventListener("submit", updateProfileData);
+closeModalButton.addEventListener("click", toggleModal);
